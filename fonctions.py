5655f739a4b3e14d381f792db7f8d2d978f08850
Tomy Besson <tomy.besson@gmail.com>
2022-02-02 17:22:09 +0100

    Ajout des fichiers de base

---
+++ fonctions.py
@@ -0,0 +1,11 @@
+# le fichier qui contient toutes les fonctions
+
+def test():
+    print("C'est ok !")
+
+
+
+
+
+if __name__ == "__main__":
+    print("Ce n'est pas ce fichier python qui doit être lancé!")
---
+++ main.py
@@ -0,0 +1,5 @@
+# fichier principal
+
+import fonctions as f
+
+f.test()
