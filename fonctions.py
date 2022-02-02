fc93d01b321b8ebc4cb9f7d7fcbb237b7b459790
tobesson1 <tomy.besson@gmail.com>
2022-02-02 17:57:35 +0100

    Ajout fonction alliance

--- fonctions.py
+++ fonctions.py
@@ -4,7 +4,11 @@ def test():
     print("C'est ok !")
 
 
+def alliance(card1, card2):
+    return card1["valeur"] == card2["valeur"]
 
+def saut_si_possible(liste_tas, num_tas):
+    pass ###
 
 
 if __name__ == "__main__":
