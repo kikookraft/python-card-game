5ca896583d48a4532d3851018e02f704f7d2582d
tobesson1 <tomy.besson@gmail.com>
2022-02-20 11:05:36 +0100

    Ajout fonction etape_reussite

--- fonctions.py
+++ fonctions.py
@@ -8,12 +8,17 @@ def alliance(card1, card2):
     return card1["valeur"] == card2["valeur"]
 
 def saut_si_possible(liste_tas, num_tas):
-    if num_tas < len(liste_tas)-1 and liste_tas[num_tas] > 0: #exclure les extrémités
+    if num_tas < len(liste_tas)-1 and num_tas > 0: #exclure les extrémités
         if liste_tas[num_tas-1]['valeur'] == liste_tas[num_tas+1]['valeur'] or \
             liste_tas[num_tas-1]['couleur'] == liste_tas[num_tas+1]['couleur']: #verifier la mm valeur/couleur
             return True
     return False
 
+def une_etape_reussite(liste_tas, pioche, affiche=False):
+    card = pioche.pop()
+    liste_tas.append(card)
+    if saut_si_possible(liste_tas, len(liste_tas)-2):
+        
 
 
 
